04947b28eb73ba41ee8ec80524d9d4f3291ff3e0
dam1 <dam1@DAM15>
2024-02-02 11:52:45 +0100

    3a modificació

--- PT3GitHub/src/main.java
+++ PT3GitHub/src/main.java
@@ -2,7 +2,8 @@
 public class main {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
-		System.out.println(“2a modificació GitHub”);
+		System.out.println("2a modificació GitHub");
+		System.out.println("3a modificació Git");
 
 	}
 
